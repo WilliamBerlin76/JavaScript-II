e271032e7ef7054cc6ccdec6a6eba09a9236a864
WilliamBerlin76 <william.berlin76@gmail.com>
2019-08-20 16:24:00 -0500

    array-methods 1-4 complete

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -74,11 +74,17 @@ console.log(firstNamesAllCaps);
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
 let runnersLargeSizeShirt = [];
+runners.filter(function(items){
+  if (items.shirt_size === "L") {runnersLargeSizeShirt.push(items)}
+});
 console.log(runnersLargeSizeShirt);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations and save the total into a ticketPriceTotal variable.
 let ticketPriceTotal = 0;
+ticketPriceTotal = runners.reduce(function(accumulator, items){
+  return accumulator + items.donation
+}, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
